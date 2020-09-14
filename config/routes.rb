438919053d05590b08085d2b11e5158beb77fdef
Benjamin Kim <benkim96@gmail.com>
2020-09-13 20:23:15 -0700

    set up routes for base app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root                      :to => "app#home"
+  get       '/home',        :to => "app#home"
+  get       '/about',       :to => "app#about"
+  get       '/contact',     :to => "app#contact"
+  get       '/profile',     :to => "app#profile"
+  get       '/signup',      :to => "users#new"
+  get       '/login',       :to => "sessions#new"
+  post      '/login',       :to => "sessions#create"
+  delete    '/login',       :to => "sessions#destroy"
+  resources                 :users
+  resources                 :posts
 end
