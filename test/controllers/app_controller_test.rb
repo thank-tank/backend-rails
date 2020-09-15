62c91a4529037e93efe5b0df47904d6eb893513f
Benjamin Kim <benkim96@gmail.com>
2020-09-15 11:24:29 -0700

    update title generation

--- test/controllers/app_controller_test.rb
+++ test/controllers/app_controller_test.rb
@@ -1,24 +1,31 @@
 require 'test_helper'
 
 class AppControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "ThankTank |"
+  end
+
   test "should get home" do
     get home_path
     assert_response :success
-  end
-
-  test "should get profile" do
-    get profile_path
-    assert_response :success
+    assert_select "title", "#{@base_title} Home"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
+    assert_select "title", "#{@base_title} About"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
+    assert_select "title", "#{@base_title} Contact"
   end
 
+  test "should get profile" do
+    get profile_path
+    assert_response :success
+    assert_select "title", "#{@base_title} Profile"
+  end
 end
