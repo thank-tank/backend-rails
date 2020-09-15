cd77afe58e30b0f0ed7602f76e68ad4a715b38d8
Benjamin Kim <benkim96@gmail.com>
2020-09-15 11:38:35 -0700

    add title helper

--- app/helpers/app_helper.rb
+++ app/helpers/app_helper.rb
@@ -1,2 +1,11 @@
 module AppHelper
+  # returns the full title, with or without title input
+  def full_title(page_title = '')
+    base_title = "ThankTank"
+    if page_title.empty?
+      base_title
+    else
+      base_title + " | " + page_title
+    end
+  end
 end
