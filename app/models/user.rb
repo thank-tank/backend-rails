a20dfffbc1f659e6f4e1d8bb0be2c4f47ae157ab
Benjamin Kim <benkim96@gmail.com>
2020-09-17 12:59:05 -0700

    add user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  EMAIL_PATTERN = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates(:name,
+            presence: true,
+            length: { maximum: 50 },
+           )
+  validates(:email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: EMAIL_PATTERN },
+            uniqueness: { case_sensitive: false },
+           )
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,49 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+
+  test "name should be present" do
+    @user.name = "       "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "       "
+    assert_not @user.email?
+  end
+
+  test "name length validation" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email length validation" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "valid email formats" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "invalid email formats" do
+    invalid_addresses = %w[user@example,com USERfoo.COM A_US-ER@@foo.bar.org first.last@foo_asdf.jp alice+bob@baz+asdf.cn]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email uniqueness" do
+    dup_user = @user.dup
+    @user.save
+    assert_not dup_user.valid?
+
+    dup_user.email = dup_user.email.upcase
+    assert_not dup_user.valid?
+  end
 end
