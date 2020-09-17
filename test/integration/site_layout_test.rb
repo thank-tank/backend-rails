0925cff5ee1ac45bfad11eff0f6d4fe16dea86f4
Benjamin Kim <benkim96@gmail.com>
2020-09-17 12:05:55 -0700

    update integ test for sign up path layout

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,7 +6,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template "app/home"
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", login_path, count: 1
-    assert_select "a[href=?]", signup_path, count: 1
+    assert_select "a[href=?]", signup_path, count: 2
     assert_select "a[href=?]", profile_path, count: 1
     assert_select "a[href=?]", about_path, count: 1
     assert_select "a[href=?]", contact_path, count: 1
@@ -15,5 +15,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get about_path
     assert_select "title", full_title("About")
+
+    get signup_path
+    assert_select "title", full_title("Sign Up")
   end
 end
