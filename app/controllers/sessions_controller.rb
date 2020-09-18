5af2f55428017b84acb1493845de185c1614da85
Benjamin Kim <benkim96@gmail.com>
2020-09-18 13:57:18 -0700

    add sessions for login and accessing current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # log in
+      log_in user
+      redirect_to user_url(user)
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
