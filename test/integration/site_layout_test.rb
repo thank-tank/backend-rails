89e856f8c98fcfa04d6a2bdf6cb328ee1b153628
Benjamin Kim <benkim96@gmail.com>
2020-09-18 15:22:50 -0700

    add valid login test with fixture users

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,10 +4,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template "app/home"
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path, count: 1
     assert_select "a[href=?]", login_path, count: 1
     assert_select "a[href=?]", signup_path, count: 2
-    assert_select "a[href=?]", profile_path, count: 1
+    assert_select "a[href=?]", profile_path, count: 0
     assert_select "a[href=?]", about_path, count: 1
     assert_select "a[href=?]", contact_path, count: 1
 
