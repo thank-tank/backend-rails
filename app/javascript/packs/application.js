b26fafec8ff8735ed7ea3ddb6ebc08363559c289
Benjamin Kim <benkim96@gmail.com>
2020-09-18 17:12:07 -0700

    add popper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("popperjs")
+require("popper")
 require("bootstrap-sprockets")
 require("turbolinks").start()
 import "bootstrap"
