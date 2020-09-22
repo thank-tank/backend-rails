061ee6cc51557cd40b6847df1453da55216cc410
Benjamin Kim <benkim96@gmail.com>
2020-09-21 20:32:19 -0700

    refactor to self class for remember digest/cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   EMAIL_PATTERN = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates(:name,
@@ -17,10 +18,22 @@ class User < ApplicationRecord
            )
   has_secure_password
 
-  # returns the hash digest of the given string
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-      BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+	# returns the hash digest of the given string
+	def digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+		BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+
+	# remember token
+	def new_token
+		SecureRandom.urlsafe_base64
+	end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
