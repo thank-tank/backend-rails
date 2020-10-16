2a051f6f9d37556b1f3042a01e02f260772e3094
Benjamin Kim <benkim96@gmail.com>
2020-10-16 13:47:29 -0700

    update sign up view

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -3,6 +3,8 @@ class AppController < ApplicationController
     if logged_in?
       @post = current_user.posts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+    else
+      @feed_items = Post.all().paginate(page: params[:page])
     end
   end
 
